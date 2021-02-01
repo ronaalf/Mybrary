0e0a2225b23702ed51d22084559cce0f00552728
Ronald Alfaro <alfaro.ronald2@gmail.com>
2021-02-01 13:33:46 -0600

    initial Setup

---
+++ server.js
@@ -0,0 +1,34 @@
+if(process.env.NODE_ENV !== 'production') {
+    require('dotenv').config();
+}
+
+const express = require('express');
+const app = express();
+const expressLayouts = require('express-ejs-layouts');
+const bodyParser = require('body-parser');
+
+    //import routes
+const indexRouter = require('./routes/index');
+const authorRouter = require('./routes/authors');
+
+app.set('view engine', 'ejs');
+app.set('views', __dirname + '/views');
+app.set('layout', 'layouts/layout');
+app.use(expressLayouts);
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ limit: '10mb', extended: false }));
+
+const mongoose = require('mongoose');
+mongoose.connect(process.env.DATABASE_URL, {useNewUrlParser: true, useUnifiedTopology: true });
+const db = mongoose.connection;
+db.on('error', error => console.error(error));
+db.once('open', () => console.log('Connected to Mongoose'));
+
+app.use('/', indexRouter);
+app.use('/authors', authorRouter);
+
+app.listen(process.env.PORT || 3000, () => console.log('Connected on port 3000'));
+
+    // https://www.youtube.com/watch?v=qj2oDkvc4dQ
+    //16:50 deployment with git, github and heroku
+    // need to finish this course #1 
